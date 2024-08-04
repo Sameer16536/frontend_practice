333d8ba47d7a2dfe25a37cb99cc08bd666e410a2
Sameer16536 <sameer.marathe16536@sakec.ac.in>
2024-08-04 22:42:27 +0530

    products added

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -1,21 +1,48 @@
-import axios from 'axios'
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 function App() {
-  const fetchProducts = async()=>{
-    const response =axios.get('https://dummyjson.com/products')
-    console.log(response.data)
-  }
+  const [products, setProducts] = useState("");
+  const [pages, setPages] = useState(1);
+  const fetchProducts = async () => {
+    const response = await axios.get("https://dummyjson.com/products");
+    console.log(response.data);
+    if (response && response.data) {
+      setProducts(response.data.products);
+    }
+  };
+  console.log(products);
 
-  useEffect(()=>{
-   fetchProducts() 
-  },[])
-  
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
     <>
-      
+      <div>
+        {products.length > 0 && (
+          <div className="products">
+            {products.slice(pages * 12 - 12, pages * 12).map((prod) => {
+              return (
+                <span className="products__single" key={prod.id}>
+                  <img src={prod.thumbnail} alt={prod.title} />
+                  <span>{prod.title}</span>
+                </span>
+              );
+            })}
+          </div>
+        )}
+        {products.length > 0 && (<div className="pagination"> 
+          <span>Prev</span>
+          {[...Array(products.length /12)].map((_,i)=>{
+            return <span key={i}>{i+1}</span>
+          })}
+          
+          <span>Next</span>
+        </div>)}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
